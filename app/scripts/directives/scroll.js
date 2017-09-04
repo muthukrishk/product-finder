64535057b7d866e79abc1dce6e6b9f73a41ae74e
jpauldhas <jpauldhas@woolworths.com.au>
2017-09-04 22:14:04 +0530

    Updated changes

--- app/scripts/directives/scroll.js
+++ app/scripts/directives/scroll.js
@@ -1,32 +1,19 @@
-angular.module('wowProductFinderApp').directive("scroll", function ($window) {
-    return function(scope, element, attrs) {
-        angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 100) {
-                 scope.ChangeClass = true;
-             } else {
-                 scope.ChangeClass = false;
-             }
-            scope.$apply();
-        });
-    };
-});
-
-
 angular.module('wowProductFinderApp').directive("scrollcustom", function ($window) {
     return function(scope, element, attrs) {
     	angular.element($window).bind("scroll", function() {
     		hdr = $('.searchLanding').height();
-    	    if (this.pageYOffset >= hdr) {
+    	    if (this.pageYOffset > hdr) {
+    	    	console.log("Fitst");
     	    	angular.element('.searchBar').addClass('fixed');
     	    	angular.element('.product-listing').css('padding-top', '250px');
     	    	angular.element('.searchBar h1.light').addClass('displaynone');
     	    	
     	    } else {
+    	    	console.log("Else");
     	    	angular.element('.searchBar').removeClass('fixed');
     	    	angular.element('.product-listing').css('padding-top', '50px');
     	    	angular.element('.searchBar h1.light').removeClass('displaynone');
     	    }
-    	   scope.$apply();
     	});
     };
 });
